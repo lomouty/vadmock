a3ce9d9539604fb3e1f3a3372cc40b80bd60650b
lomouty <lmshiyan@126.com>
2018-10-25 11:55:14 +0800

    init

---
+++ src/Mock.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the lomouty mock package.
+ *
+ * (c) lomouty <lomouty@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Lomouty\Mock;
+
+class Mock {
+
+    public function __construct() {
+
+    }
+
+    public static function mock(){
+        return 'init';
+    }
+
+}
